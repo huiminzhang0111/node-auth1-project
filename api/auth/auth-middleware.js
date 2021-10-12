f3e878ef874ee8ec6ef408b1dc4fcb1f9d6bda73
Huimin <huiminzhang0111@hotmail.com>
2021-10-11 19:16:18 -0700

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -6,6 +6,8 @@
     "message": "You shall not pass!"
   }
 */
+
+const db = require('../../data/db-config')
 function restricted(req, res, next) {
   if (req.session.user) {
     next()
@@ -25,8 +27,23 @@ function restricted(req, res, next) {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
+const checkUsernameFree = async (req, res, next) => {
+  try {
+    const existing = await db('users')
+      .where('username', req.body.username)
+      .first()
 
+    if (existing) {
+      next({
+        status: 422,
+        message: "Username taken"
+      })
+    } else {
+      next()
+    }
+  } catch(err) {
+    next(err)
+  }
 }
 
 /*
@@ -37,8 +54,22 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+const checkUsernameExists = async(req, res, next) => {
+  try {
+    const existing = await db('users')
+      .where('username', req.body.username)
+      .first()
+    if (!existing) {
+      next({
+        status: 401,
+        message: "Invalid credentials"
+      })
+    } else {
+      next()
+    }
+  } catch(err) {
+    next(err)
+  }
 }
 
 /*
@@ -50,7 +81,15 @@ function checkUsernameExists() {
   }
 */
 function checkPasswordLength() {
-
+  const { password } = req.body
+  if (password === undefined || password.length <= 3) {
+    next ({
+      status: 422,
+      message: "Password must be longer than 3 chars"
+    })
+  } else {
+    next()
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,6 +3,12 @@ const router = require('express').Router()
 const { restricted } = require('../auth/auth-middleware')
 const Users = require('./users-model')
 
+router.get('/', restricted, (req, res, next) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users)
+    }).catch(next)
+})
 /**
   [GET] /api/users
 
